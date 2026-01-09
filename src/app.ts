7795cada07ddfa7e71b235d20544bdbb84137c1a
José Padilha <jose.padilha8@gmail.com>
2026-01-09 17:20:15 -0300

    fix: app.ts

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,12 @@ import { productsRoutes } from "./modules/products/routes";
 import { stockRoutes } from "./modules/stock/routes";
 
 export const app = express();
-const port = process.env.PORT || 3333;
+
+const PORT = process.env.PORT || 3333;
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/users", usersRoutes);
 app.use("/products", productsRoutes);
 app.use("/stock", stockRoutes);
@@ -18,13 +20,11 @@ app.use("/stock", stockRoutes);
 AppDataSource.initialize()
   .then(() => {
     console.log("📦 Banco conectado com sucesso!");
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
   })
   .catch(error => {
     console.error("Erro ao inicializar o banco:", error);
   });
-
-if (process.env.NODE_ENV !== "production") {
-  app.listen(port, () => {
-    console.log("🚀 Server running on port 3333");
-  });
-}
